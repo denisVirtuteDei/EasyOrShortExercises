4a780de62fccd9f96e3829038cb50ec2dd8789a8
zero.daniel443@gmail.com <zero.daniel443@gmail.com>
2021-02-03 11:22:34 +0300

    numberToMoney

--- 2-String-functions/StringFunctions.js
+++ 2-String-functions/StringFunctions.js
@@ -3,11 +3,22 @@ console.log((function toMachineName(name, separator = '-') {
     return name.trim().split(/ \s*/).join(separator);
 })('  ijjasnd      ksndkasndk   '));
 
+//2.2
+var numberToMoney = function (n) {
+    return Number(n
+        .toString()
+        .split('.')
+        .map((val, i) => i === 1 ? val.slice(0, 2) : val)
+        .join('.')).toLocaleString();
+};
 
 //2.3
 function alphabetized(s) {
     return [...s].filter(el => el.match(/[A-Za-z]/)).sort((l, r) => l.toLowerCase().localeCompare(r.toLowerCase())).join('');
 }
+
+//2.5
+
 
 //2.6
 const alphabet = {
@@ -17,12 +28,12 @@ const alphabet = {
     'p': '16', 'q': '17', 'r': '18', 's': '19', 't': '20',
     'u': '21', 'v': '22', 'w': '23', 'x': '24', 'y': '25', 'z': '26'
 }
-  
+
 function alphabetPosition(text) {
-    return text.toLowerCase().replace(/[^a-z]/g,'').replace(/([a-z])/g, (el => alphabet[el].concat(' '))).trim();
+    return text.toLowerCase().replace(/[^a-z]/g, '').replace(/([a-z])/g, (el => alphabet[el].concat(' '))).trim();
 }
 
 //
-function duplicateCount(text){
-    return [...new Set([...text.toLowerCase()].filter((el, i, arr) => arr.slice(i+1).includes(el)))].length
+function duplicateCount(text) {
+    return [...new Set([...text.toLowerCase()].filter((el, i, arr) => arr.slice(i + 1).includes(el)))].length
 }
