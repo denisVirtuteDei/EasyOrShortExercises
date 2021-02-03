39bf7414e6b343d37cb0fa193ed3bd8b2cd98a52
zero.daniel443@gmail.com <zero.daniel443@gmail.com>
2021-02-03 11:44:12 +0300

    addFunc3-2

--- 3-Algorithms-and-functional-programming/AlgorithmsAndFunctionalProgramming.js
+++ 3-Algorithms-and-functional-programming/AlgorithmsAndFunctionalProgramming.js
@@ -1,18 +1,23 @@
 //3.1
 function flattenAndSort(array) {
-    return array.length > 0 
-      ? array.reduce((prev, val) => prev.concat(val), []).sort((l,r) => l-r) 
-      : [];
-  }
+    return array.length > 0
+        ? array.reduce((prev, val) => prev.concat(val), []).sort((l, r) => l - r)
+        : [];
+}
+
+//3.2
+function add(n) {
+    return x => x + n
+}
 
 //3.5
 function reducer(arr, callback, initValue) {
     function red(arr, callback, result, index) {
-        if(index+1 < arr.length)
-            return red(arr, callback, callback(result, arr[index]), index+1)
+        if (index + 1 < arr.length)
+            return red(arr, callback, callback(result, arr[index]), index + 1)
         return callback(result, arr[index])
     }
-    return initValue 
-        ? red(arr, callback, callback(initValue, arr[0]), 1) 
+    return initValue
+        ? red(arr, callback, callback(initValue, arr[0]), 1)
         : red(arr, callback, callback(arr[0], arr[1]), 2)
 };
